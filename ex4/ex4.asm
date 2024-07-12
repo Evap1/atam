35719e39878b9679e98eab8a15ceabead557540c
amitarad36 <129197826+amitarad36@users.noreply.github.com>
2024-07-12 22:05:27 +0300

    Update ex4.asm

--- ex4/ex4.asm
+++ ex4/ex4.asm
@@ -8,7 +8,7 @@ check_nodes:
     jge end_check              # If i >= 3, exit loop
 
     movq nodes(, %r8, 8), %r10  # Load currentNode = nodes[i]
-    
+
     # Initialize tendency
     movq $0, %r11               # 0 = undecided, 1 = up, -1 = down
     movq 0(%r10), %r12          # Load prev pointer
@@ -24,7 +24,7 @@ inner_left_check:
     movl 8(%r13), %ebx          # Load prev of prev->data
 
     # Determine tendency
-    cmp %eax, %ebx
+    cmpq %eax, %ebx
     jg set_down                 # If prev > prev of prev, set tendency to down
     jl set_up                   # If prev < prev of prev, set tendency to up
     jmp check_next_left         # If equal, check next left
@@ -43,22 +43,23 @@ check_next_left:
 
     movl 8(%r14), %ecx          # Load prev of prev of prev->data
 
-    # Check if the tendency is maintained
-    cmp %r11, $1                 # Check if tendency is up
-    je check_up
-    cmp %r11, $-1                # Check if tendency is down
-    je check_down
+    # Check tendency
+    cmpq %r11, $0
+    je continue_check           # If tendency is undecided, continue checking
+    cmpq %r11, $1
+    jl check_down               # If tendency is down, check that the values are decreasing
 
 check_up:
-    cmp %ebx, %ecx               # Compare prev of prev with prev of prev of prev
-    jge not_monotonic            # If not maintaining tendency, it's not monotonic
-    jmp next_iteration_left
+    cmpq %ebx, %ecx
+    jge not_monotonic
+    jmp continue_check
 
 check_down:
-    cmp %ebx, %ecx               # Compare prev of prev with prev of prev of prev
-    jle not_monotonic            # If not maintaining tendency, it's not monotonic
+    cmpq %ebx, %ecx
+    jle not_monotonic
+    jmp continue_check
 
-next_iteration_left:
+continue_check:
     movq %r13, %r12             # Move prev of prev to prev
     movq %r14, %r13             # Move prev of prev of prev to prev of prev
     jmp inner_left_check
@@ -82,17 +83,17 @@ inner_right_check:
     movl 8(%r13), %ebx          # Load next of next->data
 
     # Determine tendency
-    cmp %eax, %ebx
-    jg set_right_down           # If next > next of next, set tendency to down
-    jl set_right_up             # If next < next of next, set tendency to up
-    jmp check_next_right        # If equal, check next right
+    cmpq %eax, %ebx
+    jg set_right_down            # If next > next of next, set tendency to down
+    jl set_right_up              # If next < next of next, set tendency to up
+    jmp check_next_right         # If equal, check next right
 
 set_right_down:
-    movq $-1, %r11              # Set tendency to down
+    movq $-1, %r11               # Set tendency to down
     jmp check_next_right
 
 set_right_up:
-    movq $1, %r11               # Set tendency to up
+    movq $1, %r11                # Set tendency to up
 
 check_next_right:
     movq 12(%r13), %r14          # Load next of next of next pointer
@@ -101,22 +102,23 @@ check_next_right:
 
     movl 8(%r14), %ecx          # Load next of next of next->data
 
-    # Check if the tendency is maintained
-    cmp %r11, $1                 # Check if tendency is up
-    je check_right_up
-    cmp %r11, $-1                # Check if tendency is down
-    je check_right_down
+    # Check tendency
+    cmpq %r11, $0
+    je continue_check_right      # If tendency is undecided, continue checking
+    cmpq %r11, $1
+    jl check_right_down          # If tendency is down, check that the values are decreasing
 
 check_right_up:
-    cmp %ebx, %ecx               # Compare next of next with next of next of next
-    jge not_monotonic            # If not maintaining tendency, it's not monotonic
-    jmp next_iteration_right
+    cmpq %ebx, %ecx
+    jge not_monotonic
+    jmp continue_check_right
 
 check_right_down:
-    cmp %ebx, %ecx               # Compare next of next with next of next of next
-    jle not_monotonic            # If not maintaining tendency, it's not monotonic
+    cmpq %ebx, %ecx
+    jle not_monotonic
+    jmp continue_check_right
 
-next_iteration_right:
+continue_check_right:
     movq %r13, %r12             # Move next of next to next
     movq %r14, %r13             # Move next of next of next to next of next
     jmp inner_right_check
