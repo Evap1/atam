23dadf5a45f326115079e71980e35d44712f52e2
amitarad36 <129197826+amitarad36@users.noreply.github.com>
2024-07-12 14:20:02 +0300

    Update ex4.asm

--- ex4/ex4.asm
+++ ex4/ex4.asm
@@ -1,70 +1,93 @@
 .global _start
 _start:
-  movq $0, %r9                  # Initialize result to 0
+    movq $0, result               # Initialize result to 0
     movq $0, %r8                  # Initialize index i to 0
 
-check_next_node:
-    cmpq $3, %r8                  # Check if i >= 3
-    jge end_check                 # If so, exit loop
+check_nodes:
+    cmpq $3, %r8                  # Compare i with 3
+    jge end_check                 # If i >= 3, exit loop
 
     movq nodes(, %r8, 8), %r10    # Load currentNode = nodes[i]
-
-    # Initialize flags
-    movb $1, %al                  # Set hasMonotonicSeries to true
+    
+    # Check if there are at least 3 elements from the left
     movq 0(%r10), %r12            # Load prev pointer
     cmpq $0, %r12                 # Check if prev is nullptr
-    je end_check_left             # If so, end check
+    je right_check                # If so, skip checks and increment result
 
-check_left_monotonic:
-    movl 8(%r12), %eax            # Load previousValue
-    movl 8(%r10), %ebx            # Load currentValue
+    movq 0(%r12), %r13            # Load prev of prev pointer
+    cmpq $0, %r13                 # Check if prev of prev is nullptr
+    je right_check                # If so, skip checks and increment result
 
-    # Check if there's a next previous node
-    movq 0(%r12), %r13            # Load next prev pointer
-    cmpq $0, %r13                 # Check if next prev is nullptr
-    je end_check_left             # If so, end check
+    movq 0(%r13), %r14            # Load prev of prev of prev pointer
+inner_left_check:
+    cmpq $0, %r14                 # Check if prev of prev of prev is nullptr
+    je right_check                # If so, skip checks and increment result
 
-    movl 8(%r13), %ecx            # Load nextValue
+  
+    movl 8(%r12), %eax            # Load prev->data
+    movl 8(%r13), %ebx            # Load prev of prev->data
+    movl 8(%r14), %ecx            # Load prev of prev of prev->data
 
-    # Check for peak
-    cmp %ebx, %eax                # Compare currentValue and previousValue
-    jg check_trough               # If currentValue > previousValue, check trough
-    jl check_peak                  # If currentValue < previousValue, check peak
+    # Check for maxima or minima conditions
+    cmp %eax, %ebx                # Compare prev->data with prev of prev->data
+    jl check_ecx                  # If prev < prev of prev, check the next
 
-next_iteration:
-    movq %r12, %r13               # Move to the previous node
-    cmpq $0, %r13                 # Check if prev is nullptr
-    je end_check_left             # If so, end check
+    cmp %ebx, %ecx                # Compare prev of prev->data with prev of prev of prev->data
+    jl not_monotonic              # If prev of prev < prev of prev of prev, it's not monotonic
 
-    movq %r13, %r12               # Load new prev pointer
-    jmp check_left_monotonic      # Continue checking
+check_ecx:
+    cmp %eax, %ecx                # Compare prev->data with prev of prev of prev->data
+    jl not_monotonic              # If prev < prev of prev of prev, it's not monotonic
 
-check_peak:
-    cmp %eax, %ecx                # Compare previousValue and nextValue
-    jg not_monotonic              # If previousValue > nextValue, it's not monotonic
-    jmp next_iteration
+    # Move to the next previous node for continued checking
+    movq %r13, %r12               # Move prev of prev to prev
+    movq %r14, %r13               # Move prev of prev of prev to prev of prev
+    jmp inner_left_check
+right_check:
+    # Check if there are at least 3 elements to the right
+    movq 0(%r10, 8), %r12         # Load next pointer
+    cmpq $0, %r12                 # Check if next is nullptr
+    je increment_result           # If so, increment result and skip checks
 
-check_trough:
-    cmp %eax, %ecx                # Compare previousValue and nextValue
-    jl not_monotonic              # If previousValue < nextValue, it's not monotonic
-    jmp next_iteration
+    movq 0(%r12, 8), %r13         # Load next of next pointer
+    cmpq $0, %r13                 # Check if next of next is nullptr
+    je increment_result           # If so, increment result and skip checks
+
+    movq 0(%r13, 8), %r14         # Load next of next of next pointer
+inner_right_check:
+    cmpq $0, %r14                 # Check if next of next of next is nullptr
+    je increment_result           # If so, increment result and skip checks
+
+    movl 8(%r12), %eax            # Load next->data
+    movl 8(%r13), %ebx            # Load next of next->data
+    movl 8(%r14), %ecx            # Load next of next of next->data
+
+    # Check for maxima or minima conditions
+    cmp %eax, %ebx                # Compare next->data with next of next->data
+    jg check_ecx_right            # If next > next of next, check the next
+
+    cmp %ebx, %ecx                # Compare next of next->data with next of next of next->data
+    jg not_monotonic              # If next of next > next of next of next, it's not monotonic
+
+check_ecx_right:
+    cmp %eax, %ecx                # Compare next->data with next of next of next->data
+    jg not_monotonic              # If next > next of next of next, it's not monotonic
+
+    # Move to the next next node for continued checking
+    movq %r13, %r12               # Move next of next to next
+    movq %r14, %r13               # Move next of next of next to next of next
+    jmp inner_right_check
+
+increment_result:
+    incq %r9                      # Increment result if monotonic
+    incq %r8                      # Increment index i
+    jmp check_nodes               # Repeat the loop
 
 not_monotonic:
-    movb $0, %al                  # Set hasMonotonicSeries to false
-
-end_check_left:
-    # Check if left is monotonic
-    testb %al, %al                # If hasMonotonicSeries
-    jz skip_increment              # If not, skip increment
-
-    incq %r9                      # Increment result if left is monotonic
-
-skip_increment:
-    incq %r8                      # Increment i
-    jmp check_next_node
-
-end_check:
-    movq %r9, result(%rip)        # Store result
+    incq %r8                      # Increment index i
+    jmp check_nodes               # jump to next node out of the 3
+    end_check:
+    movq %r9, result              # Store the value of %r9 into the address of the label 'result'
 
 # Print "result="
     movq $1, %rax            # syscall number for sys_write
