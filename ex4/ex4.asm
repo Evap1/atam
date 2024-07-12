7b4841b4419a14bcb2588268b1b6b768b8b50049
amitarad36 <129197826+amitarad36@users.noreply.github.com>
2024-07-12 22:07:24 +0300

    Update ex4.asm

--- ex4/ex4.asm
+++ ex4/ex4.asm
@@ -24,7 +24,7 @@ inner_left_check:
     movl 8(%r13), %ebx          # Load prev of prev->data
 
     # Determine tendency
-    cmpq %eax, %ebx
+    cmp %eax, %ebx
     jg set_down                 # If prev > prev of prev, set tendency to down
     jl set_up                   # If prev < prev of prev, set tendency to up
     jmp check_next_left         # If equal, check next left
@@ -44,18 +44,18 @@ check_next_left:
     movl 8(%r14), %ecx          # Load prev of prev of prev->data
 
     # Check tendency
-    cmpq %r11, $0
+    cmp $0, %r11
     je continue_check           # If tendency is undecided, continue checking
-    cmpq %r11, $1
+    cmp $1, %r11
     jl check_down               # If tendency is down, check that the values are decreasing
 
 check_up:
-    cmpq %ebx, %ecx
+    cmp %ebx, %ecx
     jge not_monotonic
     jmp continue_check
 
 check_down:
-    cmpq %ebx, %ecx
+    cmp %ebx, %ecx
     jle not_monotonic
     jmp continue_check
 
@@ -83,7 +83,7 @@ inner_right_check:
     movl 8(%r13), %ebx          # Load next of next->data
 
     # Determine tendency
-    cmpq %eax, %ebx
+    cmp %eax, %ebx
     jg set_right_down            # If next > next of next, set tendency to down
     jl set_right_up              # If next < next of next, set tendency to up
     jmp check_next_right         # If equal, check next right
@@ -103,18 +103,18 @@ check_next_right:
     movl 8(%r14), %ecx          # Load next of next of next->data
 
     # Check tendency
-    cmpq %r11, $0
+    cmp $0, %r11
     je continue_check_right      # If tendency is undecided, continue checking
-    cmpq %r11, $1
+    cmp $1, %r11
     jl check_right_down          # If tendency is down, check that the values are decreasing
 
 check_right_up:
-    cmpq %ebx, %ecx
+    cmp %ebx, %ecx
     jge not_monotonic
     jmp continue_check_right
 
 check_right_down:
-    cmpq %ebx, %ecx
+    cmp %ebx, %ecx
     jle not_monotonic
     jmp continue_check_right
 
@@ -124,55 +124,4 @@ continue_check_right:
     jmp inner_right_check
 
 increment_result:
-    incq %r9                     # Increment result if monotonic
-    incq %r8                     # Increment index i
-    jmp check_nodes              # Repeat the loop
-
-not_monotonic:
-    incq %r8                     # Increment index i
-    jmp check_nodes              # Jump to next node out of the 3
-
-end_check:
-    movq %r9, result             # Store the value of %r9 into the address of the label 'result'
-
-
-# Print "result="
-    movq $1, %rax            # syscall number for sys_write
-    movq $1, %rdi            # file descriptor (stdout)
-    lea result_label(%rip), %rsi   # address of result_label
-    movq $8, %rdx            # number of bytes to write (length of "result=")
-    syscall                  # make the syscall to print "result="
-
-    # Convert result to ASCII character
-    movzbq result, %rax      # zero-extend result into %rax
-    add $'0', %al            # convert result value to ASCII character
-    movb %al, result_buf     # move ASCII character to result_buf
-
-    # Print the value of 'result'
-    movq $1, %rax            # syscall number for sys_write
-    movq $1, %rdi            # file descriptor (stdout)
-    lea result_buf(%rip), %rsi   # address of result_buf
-    movq $1, %rdx            # number of bytes to write (1 byte for result value)
-    syscall                  # make the syscall to print result
-
-    # Print a newline
-    movq $1, %rax            # syscall number for sys_write
-    movq $1, %rdi            # file descriptor (stdout)
-    lea newline(%rip), %rsi  # address of newline character
-    movq $1, %rdx            # number of bytes to write (1 byte for newline)
-    syscall                  # make the syscall to print newline
-
-    # Exit the program
-    movq $60, %rax           # syscall number for sys_exit
-    xor %rdi, %rdi           # exit status (0 for success)
-    syscall                  # make the syscall to exit the program
-
-.section .rodata
-result_label:
-    .asciz "result="
-newline:
-    .byte 10                 # ASCII code for newline ('\n')
-
-.section .data
-result_buf:
-    .byte ' '                # initialize with a space character (' ')
+    incq %r9                     # Increment result if mono
